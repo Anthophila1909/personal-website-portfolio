24a033730e4d65897f7f6eff6165ac0729ff9335
Anthophila1909 <elena.w@hotmail.de>
2025-03-14 15:10:51 +0100

    Imported navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import NavBar from "./NavBar";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar />
         <h1>Welcome</h1>
       </header>
     </div>
