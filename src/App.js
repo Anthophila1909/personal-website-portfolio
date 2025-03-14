7030a6efa323b1f8148f3cc70a5e94b2fd1fa3f2
Anthophila1909 <elena.w@hotmail.de>
2025-03-14 15:34:30 +0100

    Footer
    inkl. FontAwesome

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
-        <NavBar />
         <h1>Welcome</h1>
       </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="container footer">
+      <div className="row">
+        <div className="col-4">
+          <a
+            href="https://github.com/Anthophila1909"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </div>
+        <div className="col-4">© 2025 Elena Würtz</div>
+        <div className="col-4">
+          <a
+            href="https://de.linkedin.com/in/elena-w%C3%BCrtz-a47349285"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
