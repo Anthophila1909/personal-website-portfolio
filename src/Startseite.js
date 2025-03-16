28be22e0ef2851d45a7d3a9937bd8d2d1cc0d3ab
Anthophila1909 <elena.w@hotmail.de>
2025-03-16 16:18:49 +0100

    CSS: Styled "Startseite"

--- src/Startseite.js
+++ src/Startseite.js
@@ -8,23 +8,26 @@ import "./css/Startseite.css";
 export default function Startseite() {
   return (
     <div className="Startseite">
-      <div className="row mt-5 mb-5">
+      <div className="row introduction">
         <div className="col">
           <h1>
             Hi, ich bin Elena! <FontAwesomeIcon icon={faFaceSmileBeam} />
           </h1>
-          <h2>Junior Frontend Developer aus Ulm</h2>
-          <p>
+          <h2 className="berufsbezeichnung">
+            Junior Frontend Developer aus Ulm
+          </h2>
+          <p className="intro-text-anfang">
             Mein Weg in die Tech-Welt begann mit der Neugier, digitale Produkte
-            nicht nur zu nutzen, sondern selbst zu gestalten - inzwischen
-            schreibe ich mit Begeisterung Code, besonders in React.
+            nicht nur zu nutzen, sondern selbst zu gestalten. <br /> Inzwischen
+            schreibe ich mit Begeisterung Code, besonders in{" "}
+            <strong>React</strong>.
           </p>
-          <p>
+          <p className="intro-text-ende">
             Code ist für mich mehr als nur Technik - es ist mein Werkzeug, um
-            Ideen zum Leben zu erwecken. Als Junior Frontend Developerin bringe
-            ich frischen Wind, viel Motivation und einen vielseitigen Background
-            mit und bin bereit, die digitale Welt ein kleines bisschen schöner
-            und smarter zu machen.{" "}
+            Ideen zum Leben zu erwecken. <br />
+            Als Junior Frontend Developerin bringe ich frischen Wind, viel
+            Motivation und einen vielseitigen Background mit und bin bereit, die
+            digitale Welt ein kleines bisschen schöner und smarter zu machen.{" "}
             <FontAwesomeIcon icon={faWandMagicSparkles} />
           </p>
         </div>
@@ -39,7 +42,7 @@ export default function Startseite() {
       <div className="row mt-5">
         <div className="col"></div>
         <div className="col">
-          <h2>Was dich hier erwartet:</h2>
+          <h2 className="inhaltsverzeichnis">Was dich hier erwartet</h2>
           <ul>
             <li>
               Ein Einblick in <strong>meinen Werdegang</strong> und meine
@@ -57,12 +60,14 @@ export default function Startseite() {
           </ul>
         </div>
       </div>
-      <a href="/ueber-mich" className="btn btn-primary">
-        Über mich
-      </a>
-      <a href="/projekte" className="btn btn-primary">
-        Aktuelle Projekte
-      </a>
+      <div className="buttons">
+        <a href="/ueber-mich" className="btn ueber-mich">
+          Über mich
+        </a>
+        <a href="/projekte" className="btn projekte">
+          Aktuelle Projekte
+        </a>
+      </div>
     </div>
   );
 }
