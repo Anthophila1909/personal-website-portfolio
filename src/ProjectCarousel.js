af44bd6a50d630b5ec31d6d6a2ed17b7a2592f51
Anthophila1909 <elena.w@hotmail.de>
2025-03-26 10:14:28 +0100

    Meine Projekte: Adjusted imgs and added descriptions

--- src/ProjectCarousel.js
+++ src/ProjectCarousel.js
@@ -5,6 +5,9 @@ import Embroidery_Project from "./imgs/Embroidery_Project.png";
 import Python_AI_Travel_Planner from "./imgs/Python_AI_Travel_Planner.png";
 import React_Weather_App from "./imgs/React_Weather_App.png";
 import World_Clock from "./imgs/World_Clock.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHandPointer } from "@fortawesome/free-solid-svg-icons";
+
 import "./css/ProjectCarousel.css";
 
 export default function ProjectCarousel() {
@@ -18,85 +21,101 @@ export default function ProjectCarousel() {
             alt="First slide"
           />
           <Carousel.Caption>
-            <h3>
+            <div className="projekt-titel">
               <a
                 href="https://ai-fantasy-figure-generator.netlify.app/"
                 target="_blank"
                 rel="noreferrer"
+                className=" btn projekt-btn"
               >
-                AI Fantasy Figure Generator
+                AI Fantasy Figure Generator{" "}
+                <FontAwesomeIcon icon={faHandPointer} />
               </a>
-            </h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div>
+            <p>
+              Eine KI-App, die eine zufällige Fantasy-Figur generiert. Sie wurde
+              mit <strong>JS</strong> erstellt und verwendet die AI-API von
+              SheCodes.
+            </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={7000}>
           <img src={React_Weather_App} className="w-50" alt="Second slide" />
           <Carousel.Caption>
-            <h3>
-              {" "}
+            <div className="projekt-titel">
               <a
                 href="https://elenas-react-weather-app.netlify.app/"
                 target="_blank"
                 rel="noreferrer"
+                className=" btn projekt-btn"
               >
-                Weather App
+                Weather App <FontAwesomeIcon icon={faHandPointer} />
               </a>
-            </h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <p>
+              Eine Wetter App die das aktuelle Wetter und die Vorhersage für die
+              nächsten 5 Tage anzeigt. Sie wurde mit <strong>React.js</strong>{" "}
+              erstellt und verwendet die OpenWeather API von SheCodes.
+            </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={7000}>
           <img src={World_Clock} className="w-50" alt="Third slide" />
           <Carousel.Caption>
-            <h3>
-              {" "}
+            <div className="projekt-titel">
               <a
                 href="https://world-clock-by-elena.netlify.app/"
                 target="_blank"
                 rel="noreferrer"
+                className=" btn projekt-btn"
               >
-                World Clock
+                World Clock <FontAwesomeIcon icon={faHandPointer} />
               </a>
-            </h3>
+            </div>
             <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              Eine Weltuhr, die die aktuelle Uhrzeit in verschiedenen Städten
+              anzeigt. Sie wurde mit <strong>JS</strong> erstellt und verwendet
+              die World Time API von SheCodes.
             </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={7000}>
           <img src={Dictionary_App} className="w-50" alt="Fourth slide" />
           <Carousel.Caption>
-            <h3>
-              {" "}
+            <div className="projekt-titel">
               <a
                 href="https://the-dictionary-application.netlify.app/"
                 target="_blank"
                 rel="noreferrer"
+                className=" btn projekt-btn"
               >
-                Dictionary Application
+                Dictionary Application <FontAwesomeIcon icon={faHandPointer} />
               </a>
-            </h3>
+            </div>
             <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              Ein Wörterbuch, das die Definition eines Wortes anzeigt. Es wurde
+              mit <strong>React.js</strong> erstellt und verwendet die
+              Dictionary API von SheCodes.
             </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={7000}>
           <img src={Embroidery_Project} className="w-50" alt="Fifth slide" />
           <Carousel.Caption>
-            <h3>
-              {" "}
+            <div className="projekt-titel">
               <a
                 href="https://elenas-embroidery-project.netlify.app/"
                 target="_blank"
                 rel="noreferrer"
+                className=" btn projekt-btn"
               >
-                Embroidery Project
+                Embroidery Project <FontAwesomeIcon icon={faHandPointer} />
               </a>
-            </h3>
+            </div>
             <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              Eine Website über ein Hobby-Projekt. Sie wurde mit{" "}
+              <strong>JS</strong> erstellt und hat ein{" "}
+              <strong>responsives Design</strong>.
             </p>
           </Carousel.Caption>
         </Carousel.Item>
@@ -107,18 +126,21 @@ export default function ProjectCarousel() {
             alt="Sixth slide"
           />
           <Carousel.Caption>
-            <h3>
-              {" "}
+            <div className="projekt-titel">
               <a
                 href="https://colab.research.google.com/drive/1urP2VOVLvP4JfFNrWz3yMbqMOREOEscj?usp=sharing"
                 target="_blank"
                 rel="noreferrer"
+                className=" btn projekt-btn"
               >
-                AI Travel Planner
+                AI Travel Planner <FontAwesomeIcon icon={faHandPointer} />
               </a>
-            </h3>
+            </div>
             <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              Ein KI-Reiseplaner, der die beste Reiseroute für eine Reise
+              zwischen verschiedenen Städten berechnet. Er wurde mit{" "}
+              <strong>Python</strong>, der AI-API und der OpenWeather API von
+              SheCodes erstellt.
             </p>
           </Carousel.Caption>
         </Carousel.Item>
